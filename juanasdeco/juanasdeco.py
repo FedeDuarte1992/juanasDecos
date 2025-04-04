20d62332ec435b2df8024db423f8b5dda0ea2662
FedeDuarte1992 <124315674+FedeDuarte1992@users.noreply.github.com>
2025-04-03 21:24:09 -0300

    prueba

--- juanasdeco/juanasdeco.py
+++ juanasdeco/juanasdeco.py
@@ -1,39 +1,22 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-
-import reflex as rx
-
-from rxconfig import config
-
+import reflex as rx  # o import pynecone as pc (dependiendo de la versión)
 
 class State(rx.State):
-    """The app state."""
-
-    ...
-
+    pass
 
 def index() -> rx.Component:
-    # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
-        rx.logo(),
+    return rx.center(
+        rx.text("¡Hola Mundo!", font_size="2em"),
+        rx.image(src="https://reflex.dev/logo.jpg",alt="Reflex Logo",)
+        rx.button(
+            "Fancy Button",
+            border_radius="1em",
+            box_shadow="rgba(151, 65, 252, 0.8) 0 15px 30px -10px",
+            background_image="linear-gradient(144deg,#AF40FF,#5B42F3 50%,#00DDEB)",
+            box_sizing="border-box",
+            color="white",
+            opacity=1,
+                )
     )
 
-
-app = rx.App()
-app.add_page(index)
+app = rx.App()  # o pc.App() si usas Pynecone
+app.add_page(index, route="/")  # Página principal
